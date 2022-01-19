8389faa890b6551581932f4056041c9f017720a5
qq2635 <qq2635@naver.com>
2022-01-19 22:40:36 +0900

    JDBC 첫번째연동

---
+++ src/main/java/springboard/command/ListCommand.java
@@ -0,0 +1,67 @@
+package springboard.command;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
+
+import springboard.model.JDBCTemplateDAO;
+import springboard.model.SpringBbsDTO;
+/*
+ BbsCommandImpl 인터페이스를 구현했으므로 execute()는 반드시
+ 오버라이딩 해야한다. 또한 해당 객체는 부모타입인 BbsCommandImpl로
+ 참조할 수 있다.
+ */
+public class ListCommand implements BbsCommandImpl{
+
+	@Override
+	public void execute(Model model) {
+		
+		System.out.println("Listcommand> execute() 호출");
+		/*
+		 컨트롤러에서 인자로 전달한 Model객체에는 request객체가 저장되어
+		 있다. asMap()을 통해 Map컬렉션으로 변환한 후 모든 요청을 얻어온다.
+		 */
+		Map<String, Object> paramMap = model.asMap();
+		//Map에 저장될때 현재 Object형으로 저장된 request객체를 원래의 형으로 형변환 해준다. 
+		HttpServletRequest req =
+				(HttpServletRequest)paramMap.get("req");
+		
+		JDBCTemplateDAO  dao = new JDBCTemplateDAO();
+		//검색어 처리
+		String addQueryString ="";
+		//request 내장객체를 통해 폼값을 받아온다.
+		String searchColumn = req.getParameter("searchColumn");
+		String searchWord = req.getParameter("searchWord");
+		if(searchWord != null) {
+			//검색어가 있는 경우 쿼리스트링 추가
+			addQueryString = String.format("searchColumn=%s&searchWord=%s&",searchColumn,searchWord);
+			//DAO로 전달할 데이터를 Map컬렉션에 저장
+			paramMap.put("Column",searchColumn);
+			paramMap.put("Word", searchWord);
+		}
+		
+		//전체 레코드 수 카운트하기
+		int totalRecordCount = dao.getTotalCount(paramMap);
+		
+		//실제 출력할 게시물을 select 한 후 반환받음(페이징 X)
+		ArrayList<SpringBbsDTO> listRows = dao.list(paramMap);
+		
+		//목록에 출력할 게시물의 가상번호 계산하여 부여하기
+		int virtualNum = 0;
+		int countNum = 0;
+		
+		for(SpringBbsDTO row: listRows) {
+			//전체 게시물의 갯수에서 하나씩 차감하면서 가상번호를 부여한다.(페이징x)
+			virtualNum = totalRecordCount --;
+			//가상번호를 setter를 통해 저장
+			row.setVirtualNum(virtualNum);
+		}
+		//위에서 처리한 목록의 모든 처리겨로가를 Model에 저장한다.
+		model.addAttribute("listRows",listRows);
+		//JdbcTemplate을 사용할때는 자원반납을 하지않는다.
+		//dao.close();
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<h1>스프링 MVC시작하기</h1>
+	<h2>Spring-JDBC이용한 게시판 제작</h2>
+	<li>
+		<a href="board/list.do" target="_blank">
+			JdbcTemplate으로 게시판 바로가기
+		</a>
+	</li>
+</body>
+</html>
